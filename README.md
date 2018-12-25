7f7d7ed42f560058dadc7b06ca0bc6cca224a971
Thomas Hawtin <tom@Thomass-MacBook-Air.local>
2018-12-25 14:33:51 +0000

    Merry Christmas

---
+++ README.md
@@ -0,0 +1,5 @@
+What better Christmas present than a poorly thought idea that I've not even bothered trying to use myself.
+
+You're welcome.
+
+Use at your own risk.
---
+++ src/tackline/orphan/Sleaze.java
@@ -0,0 +1,104 @@
+package tackline.orphan;
+
+public final class Sleaze {
+   private Sleaze() {
+   }
+   /**
+    * Do your worst.
+    */
+   @FunctionalInterface
+   public interface Sleazy<R, EXC extends Throwable> {
+      void sleaze(Sleazer<R, EXC> sleazer);
+   }
+   /**
+    * Choose only one.
+    */
+   public interface Sleazer<R, EXC extends Throwable> {
+      /**
+       * Can't make up your mind what to get.
+       */
+      void of(SleazeSupplier<R, EXC> fn);
+      /**
+       * Nice path - pass a present.
+       */
+      void return_(R value);
+      /**
+       * Naughty path - throw coal.
+       */
+      void throw_(EXC exc);
+   }
+   /**
+    * Like a pre-2018 Santa.
+    */
+   @FunctionalInterface
+   public interface SleazeSupplier<R, EXC extends Throwable> {
+      R get() throws EXC;
+   }
+    
+   private enum State {
+      UNSET, RETURN, THROW
+   }  
+   /**
+    * Allows returning values and throws (one type of) exception
+    *    through a chimney stack
+    *    given a void returning, non-throwing interface.
+    * 
+    * Usage something like:
+    * <pre>
+    *   Integer result = sleaze(IOException.class, sleaze -> {
+    *      things.unbox(thing -> {
+    *      if (x) {
+    *         sleaze.of(() -> func());
+    *      } else if (y) {
+    *         sleaze.return_(42);
+    *      } else {
+    *         sleaze.throw_(new IOException());
+    *      }
+    *      });
+    *   });
+    * </pre>
+    */
+   public static <R, EXC extends Throwable> R sleaze(Class<EXC> excClass, Sleazy<R, EXC> sleazy) throws EXC {
+      class SleazerImpl implements Sleazer<R, EXC> {
+         State state = State.UNSET;
+         R value;
+         EXC exc;
+         public void of(SleazeSupplier<R, EXC> fn) {
+            try {
+                return_(fn.get());
+            } catch (Throwable exc) {
+               if (exc.getClass().isAssignableFrom(excClass)) { // !! CHECK !!
+                  throw_(excClass.cast(exc));
+               } else if (exc instanceof Error) {
+                  throw (Error)exc;
+               } else if (exc instanceof RuntimeException) {
+                  throw (RuntimeException)exc;
+               } else {
+                  throw new IllegalArgumentException("Undeclared exception thrown", exc);
+               } 
+            }
+         }
+         public void return_(R value) {
+            if (state != State.UNSET) {
+               throw new IllegalStateException();
+            }
+            this.value = value;
+            this.state = State.RETURN;
+         }
+         public void throw_(EXC exc) {
+            if (state != State.UNSET) {
+               throw new IllegalStateException();
+            }
+            this.exc = exc;
+            this.state = State.THROW;
+         }
+      }
+      SleazerImpl sleazer = new SleazerImpl();
+      sleazy.sleaze(sleazer);
+      switch (sleazer.state) {
+         case RETURN: return sleazer.value;
+         case THROW: throw sleazer.exc;
+         case UNSET: default: throw new IllegalStateException("Expected a return or a throw");
+      }
+   }
+}
